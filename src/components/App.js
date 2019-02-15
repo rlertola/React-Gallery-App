7e8d346b3c2c2641dd4bc4dc3bde77af37463110
Ryan Lertola <rlertola@gmail.com>
2019-02-14 22:35:17 -0600

    Project setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './css/index.css';
+import '../css/index.css';
+
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Header />
       </div>
     );
   }
